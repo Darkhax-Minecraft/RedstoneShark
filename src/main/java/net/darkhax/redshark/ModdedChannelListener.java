01ddc84d347ebf42a11ab4c5518119e545e71078
Jon McManus <due@wxwhatever.com>
2019-03-14 15:04:09 +1000

    Some adjustments to the choice of packet logging location.
    Similarly to "latest.log", .0, .1., ... .<infinity> will be added to the
    filename to denote subsequent reports.

--- src/main/java/net/darkhax/redshark/ModdedChannelListener.java
+++ src/main/java/net/darkhax/redshark/ModdedChannelListener.java
@@ -1,37 +1,35 @@
 package net.darkhax.redshark;
 
-import java.io.File;
-import java.io.FileWriter;
-
-import org.apache.commons.lang3.SystemUtils;
-
-import gnu.trove.map.hash.TByteObjectHashMap;
 import io.netty.buffer.ByteBufUtil;
 import io.netty.channel.ChannelHandler.Sharable;
-import it.unimi.dsi.fastutil.bytes.Byte2ObjectMap;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import it.unimi.dsi.fastutil.bytes.Byte2ObjectMap;
 import net.minecraftforge.fml.common.network.FMLIndexedMessageToMessageCodec;
 import net.minecraftforge.fml.common.network.internal.FMLProxyPacket;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleIndexedCodec;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
+import org.apache.commons.lang3.SystemUtils;
+
+import java.io.File;
+import java.io.FileWriter;
 
 @Sharable
 public class ModdedChannelListener extends SimpleChannelInboundHandler<FMLProxyPacket> {
 
     private final Byte2ObjectMap<Class<?>> discriminators;
 
-    public ModdedChannelListener (SimpleIndexedCodec codec) {
+    public ModdedChannelListener(SimpleIndexedCodec codec) {
 
         this.discriminators = ReflectionHelper.getPrivateValue(FMLIndexedMessageToMessageCodec.class, codec, "discriminators");
     }
 
     @Override
-    protected void channelRead0 (ChannelHandlerContext ctx, FMLProxyPacket msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, FMLProxyPacket msg) throws Exception {
 
         try {
 
-            try (FileWriter writer = new FileWriter(new File("packets.log"), true)) {
+            try (FileWriter writer = new FileWriter(new File(RedShark.filename), true)) {
 
                 final byte[] bytes = ByteBufUtil.getBytes(msg.payload());
 
@@ -39,9 +37,7 @@ public class ModdedChannelListener extends SimpleChannelInboundHandler<FMLProxyP
 
                 writer.write(line + SystemUtils.LINE_SEPARATOR);
             }
-        }
-
-        catch (final Exception e) {
+        } catch (final Exception e) {
 
             RedShark.log.catching(e);
         }
--- src/main/java/net/darkhax/redshark/RedShark.java
+++ src/main/java/net/darkhax/redshark/RedShark.java
@@ -1,12 +1,5 @@
 package net.darkhax.redshark;
 
-import java.util.EnumMap;
-import java.util.Map;
-import java.util.Map.Entry;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import io.netty.channel.ChannelHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -16,14 +9,38 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleIndexedCodec;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.fml.relauncher.Side;
+import org.apache.commons.io.filefilter.NameFileFilter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.File;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 @Mod(modid = "redshark", name = "Red Shark", version = "@VERSION@", acceptableRemoteVersions = "*", certificateFingerprint = "@FINGERPRINT@")
 public class RedShark {
 
     public static final Logger log = LogManager.getLogger("Red Shark");
 
+    public static String filename;
+
     @EventHandler
-    public void loadComplete (FMLLoadCompleteEvent event) {
+    public void loadComplete(FMLLoadCompleteEvent event) {
+        String[] files = new File(".").list(new NameFileFilter("debug.log"));
+        if (files != null && files.length == 0) {
+            filename = "debug.log";
+        } else if (files != null) {
+            int max = -1;
+            for (String s : files) {
+                String[] dots = s.split("\\.");
+                if (!dots[dots.length - 1].equals("log")) {
+                    int nMax = Integer.parseInt(dots[dots.length - 1]);
+                    max = Math.max(max, nMax);
+                }
+            }
+            filename = String.format("debug.log.%d", max + 1);
+        }
 
         final EnumMap<Side, Map<String, FMLEmbeddedChannel>> channels = ReflectionHelper.getPrivateValue(NetworkRegistry.class, NetworkRegistry.INSTANCE, "channels");
 
